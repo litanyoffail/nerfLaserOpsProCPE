defccf0a76065bb80e917b8c6855c378b5ff9a6b
litanyoffail <122475151+litanyoffail@users.noreply.github.com>
2025-06-07 23:12:35 -0400

    Add files via upload

--- nerfLaserOpsProCPE.ino
+++ nerfLaserOpsProCPE.ino
@@ -15,123 +15,175 @@
 
 Adafruit_CPlay_NeoPixel strip = Adafruit_CPlay_NeoPixel(10, CPLAY_NEOPIXELPIN, NEO_GRB + NEO_KHZ800);
 
-static const unsigned long ANIMATION_DELAY = 150; //ms
-static const unsigned long HOLD_DELAY = 300; //ms
-
-// static const long purpleTeam[2] = {alphaPointPurple, deltaBurstPurple};
-// static const long redTeam[2] = {alphaPointRed, deltaBurstRed};
-// static const long blueTeam[2] = {alphaPointBlue, deltaBurstBlue};
+int ANIMATION_DELAY = 200;
+int HOLD_DELAY = 350;
 
 // Purple Team = 0
 // Red Team = 1
 // Blue Team = 2
-int ownerTeam = 1;
+int ownerTeam = 0;
+
+// Hit Points and Damage
+int hitPointMax = 10;
+int hitPoint = hitPointMax;
 
 // Hits will only be valid from opposing team if Red or Blue, or any Purple
 // Prevents Red/Blue from affecting Purple and vice versa to copy stock behaviour.
 // Maybe there is a game mode that can be tied to modyfing this behaviour somehow?
 bool validHit() {
   if(myDecoder.protocolNum==UNKNOWN) {
-    if (ownerTeam == 0) {
-      if (myDecoder.value == alphaPointPurple) {
-        return true;
-      }
-      else if (myDecoder.value == deltaBurstPurple) {
-        return true;
-      }
-    }
-    else if (ownerTeam == 1) {
-      if (myDecoder.value == alphaPointBlue) {
-        return true;
-      }
-      else if (myDecoder.value == deltaBurstBlue) {
-        return true;
-      }
-    }
-    else if (ownerTeam == 2) {
-      if (myDecoder.value == alphaPointRed) {
-        return true;
-      }
-      else if (myDecoder.value== deltaBurstRed) {
-        return true;
-      }
+    switch (ownerTeam) {
+      case 0:
+        if (myDecoder.value == alphaPointPurple || myDecoder.value == deltaBurstPurple) {
+          return true;
+        }
+        break;
+      case 1:
+        if (myDecoder.value == alphaPointBlue || myDecoder.value == deltaBurstBlue) {
+          return true;
+        }
+        break;
+      case 2:
+        if (myDecoder.value == alphaPointRed || myDecoder.value == deltaBurstRed) {
+          return true;
+        }
+        break;
     }
     return false;
   }
 }
 
 void validHitResponse() {
-  Serial.print("VALID HIT 0x");
-  Serial.println(myDecoder.value, HEX);
-  setLEDPurple ();
+  ANIMATION_DELAY = 0; //ms
+  HOLD_DELAY = 250; //ms
+  --hitPoint;
+  switch (ownerTeam) {
+    case 0:
+      Serial.print("VALID HIT PURPLE 0x");
+      Serial.println(myDecoder.value, HEX);
+      setLEDPurple();
+      break;
+    case 1:
+      Serial.print("VALID HIT BLUE 0x");
+      Serial.println(myDecoder.value, HEX);
+      setLEDBlue();
+      break;
+    case 2:
+      Serial.print("VALID HIT RED 0x");
+      Serial.println(myDecoder.value, HEX);
+      setLEDRed();
+      break;
+  }
   delay(HOLD_DELAY);
-  strip.clear();
-  strip.show();
+  setLEDNone();
 }
 
 // Cycle ownerTeam variable between teams and show a boot-up animation to indicate ownerTeam color when Right Button is pressed.
-//void teamSwitchButton() {
-//  if (ownerTeam == 0) {
-//    for (int i=0; i<strip.numPixels(); i++) {
-//      strip.setPixelColor(i, 0xFF, 0x00, 0xFF);
-//      strip.show();
-//      delay(ANIMATION_DELAY);
-//    }
-//    delay(HOLD_DELAY);
-//    strip.clear();
-//    strip.show();
-//    delay(HOLD_DELAY);
-//  }
-//  else if (ownerTeam == 1) {
-//    for (int i=0; i<strip.numPixels(); i++) {
-//      strip.setPixelColor(i, 0xFF, 0x00, 0x00);
-//      strip.show();
-//      delay(ANIMATION_DELAY);
-//    }
-//    delay(HOLD_DELAY);
-//    strip.clear();
-//    strip.show();
-//    delay(HOLD_DELAY);
-//  }
-//  else if (ownerTeam == 2) {
-//    for (int i=0; i<strip.numPixels(); i++) {
-//      strip.setPixelColor(i, 0x00, 0x00, 0xFF);
-//      strip.show();
-//      delay(ANIMATION_DELAY);
-//    }
-//    delay(HOLD_DELAY);
-//    strip.clear();
-//    strip.show();
-//    delay(HOLD_DELAY);
-//  }
-//}
+void teamSwitchButton() {
+  ANIMATION_DELAY = 180;
+  HOLD_DELAY = 500;
+  setLEDNone();
+  if (ownerTeam < 2) {
+    ++ownerTeam;
+  }
+  else {
+    ownerTeam = 0;
+  }
+  switch (ownerTeam) {
+    case 0:
+      setLEDPurple();
+      break;
+    case 1:
+      setLEDRed();
+      break;
+    case 2:
+      setLEDBlue();
+      break;
+  }
+  delay(HOLD_DELAY);
+  hitPointLEDGreen();
+}
 
 // Reset the unit to full health on current ownerTeam when Left Button is pressed.
-//void resetButton() {
-//}
+void resetButton() {
+  hitPoint = hitPointMax;
+  setLEDNone();
+  switch (ownerTeam) {
+    case 0:
+      setLEDPurple();
+      break;
+    case 1:
+      setLEDRed(); 
+      break;
+    case 2:
+      setLEDBlue();
+      break;
+  }
+  hitPointLEDGreen();
+}
 
 // Set All NeoPixels to Purple
 void setLEDPurple () {
   for (int i=0; i<strip.numPixels(); i++) {
     strip.setPixelColor(i, 0xFF, 0x00, 0xFF);
+    strip.show();
+    delay(ANIMATION_DELAY);
   }
-  strip.show();
 }
 
 // Set All NeoPixels to Red
 void setLEDRed () {
   for (int i=0; i<strip.numPixels(); i++) {
     strip.setPixelColor(i, 0xFF, 0x00, 0x00);
+    strip.show();
+    delay(ANIMATION_DELAY);
   }
-  strip.show();
 }
 
 // Set All NeoPixels to Blue
-void setLEDBlue () {
+void setLEDBlue() {
   for (int i=0; i<strip.numPixels(); i++) {
     strip.setPixelColor(i, 0x00, 0x00, 0xFF);
+    strip.show();
+    delay(ANIMATION_DELAY);
   }
+}
+
+void setLEDGreen() {
+  for (int i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, 0x00, 0xFF, 0x00);
+    strip.show();
+    delay(ANIMATION_DELAY);
+  }
+}
+
+void hitPointLEDGreen() {
+  setLEDNone();
+  for (int i=0; i<hitPoint; i++) {
+    strip.setPixelColor(i, 0x00, 0xFF, 0x00);
+    strip.show();
+    delay(ANIMATION_DELAY);
+  }
+}
+
+void setLEDNone() {
+  strip.clear();
   strip.show();
+}
+
+void noHitPoint() {
+  ANIMATION_DELAY = 1000;
+//  while (hitPoint == 0) {
+    for (int i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, 0xFF, 0xFF, 0xFF);
+      strip.show();
+      delay(ANIMATION_DELAY);
+    }
+    setLEDNone();
+    if (CircuitPlayground.rightButton()) {
+      resetButton();
+    }
+//  }
 }
 
 void setup() {
@@ -141,19 +193,37 @@ void setup() {
   myReceiver.enableIRIn(); // Start the receiver
   Serial.println(F("Ready to receive IR signals"));
   strip.begin();
-  strip.setBrightness(64);
+  strip.setBrightness(42);
   strip.clear();
   strip.show();
+  setLEDPurple();
+  setLEDGreen();
 }
 
 void loop() {
     // Activate IR Receiver
-  if(myReceiver.getResults()) {
+  if (myReceiver.getResults()) {
     myDecoder.decode();
-    if(validHit() == true) {
-      validHitResponse ();
+    if (validHit() == true && hitPoint >= 2) {
+      validHitResponse();
+      hitPointLEDGreen();
     }
-    myReceiver.enableIRIn();
+    else if (validHit() == true && hitPoint == 1) {
+      validHitResponse();
+      noHitPoint();
+    }
+    else {
+      Serial.print("INVALID HIT 0x");
+      Serial.println(myDecoder.value, HEX);
+    }
   }
-  
+  myReceiver.enableIRIn();
+
+  if (CircuitPlayground.rightButton()) {
+    teamSwitchButton();
+  }
+
+  if (CircuitPlayground.leftButton()) {
+    resetButton();
+  }
 }
