efae6f83d092a997c6622186260033fa7c691e72
Aaron Mavrinac <mavrinac@gmail.com>
2025-06-09 09:25:47 -0400

    Consolidate and compactify various things.

--- nerfLaserOpsProCPE.ino
+++ nerfLaserOpsProCPE.ino
@@ -5,22 +5,27 @@
 #define myDecoder CircuitPlayground.irDecoder
 
 /* IR hashes for Nerf LaserOps Pro standard game */
-#define alphaPointPurple 0x67228B44
-#define alphaPointRed 0x78653B0E
-#define alphaPointBlue 0x2FFEA610
+#define AP_PURPLE 0x67228B44
+#define AP_RED 0x78653B0E
+#define AP_BLUE 0x2FFEA610
 
-#define deltaBurstPurple 0xD303E9B8
-#define deltaBurstRed 0xE4469982
-#define deltaBurstBlue 0x9BE00484
+#define DB_PURPLE 0xD303E9B8
+#define DB_RED 0xE4469982
+#define DB_BLUE 0x9BE00484
 
 #define PURPLE 0x00FF00FF
 #define RED 0x00FF0000
 #define GREEN 0x0000FF00
 #define BLUE 0x000000FF
 
+const char* teamName[] = {"PURPLE", "RED", "BLUE"};
+const uint32_t teamColor[] = {PURPLE, RED, BLUE};
+
 #define HOLD_DELAY_VALID_HIT 250
 #define HOLD_DELAY_TEAM_SWITCH 500
+#define ANIMATION_DELAY_SETUP 180
 #define ANIMATION_DELAY_TEAM_SWITCH 180
+#define ANIMATION_DELAY_RESET 200
 #define ANIMATION_DELAY_HIT_POINT 200
 #define ANIMATION_DELAY_NO_HIT_POINT 1000
 
@@ -32,54 +37,30 @@ Adafruit_CPlay_NeoPixel strip = Adafruit_CPlay_NeoPixel(10, CPLAY_NEOPIXELPIN, N
 int ownerTeam = 0;
 
 // Hit Points and Damage
-int hitPointMax = 10;
-int hitPoint = hitPointMax;
+#define HIT_POINT_MAX 10
+int hitPoint = HIT_POINT_MAX;
 
 // Hits will only be valid from opposing team if Red or Blue, or any Purple
 // Prevents Red/Blue from affecting Purple and vice versa to copy stock behaviour.
 // Maybe there is a game mode that can be tied to modyfing this behaviour somehow?
 bool validHit() {
-  if(myDecoder.protocolNum==UNKNOWN) {
-    switch (ownerTeam) {
-      case 0:
-        if (myDecoder.value == alphaPointPurple || myDecoder.value == deltaBurstPurple) {
-          return true;
-        }
-        break;
-      case 1:
-        if (myDecoder.value == alphaPointBlue || myDecoder.value == deltaBurstBlue) {
-          return true;
-        }
-        break;
-      case 2:
-        if (myDecoder.value == alphaPointRed || myDecoder.value == deltaBurstRed) {
-          return true;
-        }
-        break;
-    }
-    return false;
-  }
+  const uint32_t alphaPoint[] = {AP_PURPLE, AP_BLUE, AP_RED};
+  const uint32_t deltaBurst[] = {DB_PURPLE, DB_BLUE, AP_RED};
+
+  return myDecoder.protocolNum == UNKNOWN &&
+         (myDecoder.value == alphaPoint[ownerTeam] ||
+          myDecoder.value == deltaBurst[ownerTeam]);
 }
 
 void validHitResponse() {
   --hitPoint;
-  switch (ownerTeam) {
-    case 0:
-      Serial.print("VALID HIT PURPLE 0x");
-      Serial.println(myDecoder.value, HEX);
-      setLEDColor(PURPLE, 0);
-      break;
-    case 1:
-      Serial.print("VALID HIT BLUE 0x");
-      Serial.println(myDecoder.value, HEX);
-      setLEDColor(BLUE, 0);
-      break;
-    case 2:
-      Serial.print("VALID HIT RED 0x");
-      Serial.println(myDecoder.value, HEX);
-      setLEDColor(RED, 0);
-      break;
-  }
+
+  Serial.print(F("VALID HIT "));
+  Serial.print(teamName[ownerTeam]);
+  Serial.print(F(" 0x"));
+  Serial.println(myDecoder.value, HEX);
+
+  setLEDColor(teamColor[ownerTeam], 0);
   delay(HOLD_DELAY_VALID_HIT);
   setLEDNone();
 }
@@ -87,42 +68,19 @@ void validHitResponse() {
 // Cycle ownerTeam variable between teams and show a boot-up animation to indicate ownerTeam color when Right Button is pressed.
 void teamSwitchButton() {
   setLEDNone();
-  if (ownerTeam < 2) {
-    ++ownerTeam;
-  }
-  else {
+  if (++ownerTeam > 2) {
     ownerTeam = 0;
   }
-  switch (ownerTeam) {
-    case 0:
-      setLEDColor(PURPLE, ANIMATION_DELAY_TEAM_SWITCH);
-      break;
-    case 1:
-      setLEDColor(RED, ANIMATION_DELAY_TEAM_SWITCH);
-      break;
-    case 2:
-      setLEDColor(BLUE, ANIMATION_DELAY_TEAM_SWITCH);
-      break;
-  }
+  setLEDColor(teamColor[ownerTeam], ANIMATION_DELAY_TEAM_SWITCH);
   delay(HOLD_DELAY_TEAM_SWITCH);
   hitPointLEDGreen();
 }
 
 // Reset the unit to full health on current ownerTeam when Left Button is pressed.
 void resetButton() {
-  hitPoint = hitPointMax;
+  hitPoint = HIT_POINT_MAX;
   setLEDNone();
-  switch (ownerTeam) {
-    case 0:
-      setLEDColor(PURPLE);
-      break;
-    case 1:
-      setLEDColor(RED);
-      break;
-    case 2:
-      setLEDColor(BLUE);
-      break;
-  }
+  setLEDColor(teamColor[ownerTeam], ANIMATION_DELAY_RESET);
   hitPointLEDGreen();
 }
 
@@ -140,32 +98,27 @@ void setLEDColor(uint32_t color, int animation_delay)
   }
 }
 
-void hitPointLEDGreen() {
+void showHitPointLED(int hp) {
   setLEDNone();
-  for (int i=0; i<hitPoint; i++) {
-    strip.setPixelColor(i, 0x00, 0xFF, 0x00);
-    strip.show();
-    delay(ANIMATION_DELAY_HIT_POINT);
-  }
-}
-
-void setLEDNone() {
-  strip.clear();
-  strip.show();
-}
-
-void noHitPoint() {
-//  while (hitPoint == 0) {
+  if (hp > 0) {
+      for (int i=0; i<hp; i++) {
+        strip.setPixelColor(i, 0x00, 0xFF, 0x00);
+        strip.show();
+        delay(ANIMATION_DELAY_HIT_POINT);
+      }
+  } else {
     for (int i=0; i<strip.numPixels(); i++) {
       strip.setPixelColor(i, 0xFF, 0xFF, 0xFF);
       strip.show();
       delay(ANIMATION_DELAY_NO_HIT_POINT);
     }
     setLEDNone();
-    if (CircuitPlayground.rightButton()) {
-      resetButton();
-    }
-//  }
+  }
+}
+
+void setLEDNone() {
+  strip.clear();
+  strip.show();
 }
 
 void setup() {
@@ -178,24 +131,20 @@ void setup() {
   strip.setBrightness(42);
   strip.clear();
   strip.show();
-  setLEDColor(PURPLE);
-  setLEDColor(GREEN);
+  setLEDColor(PURPLE, ANIMATION_DELAY_SETUP);
+  setLEDColor(GREEN, ANIMATION_DELAY_SETUP);
 }
 
 void loop() {
     // Activate IR Receiver
   if (myReceiver.getResults()) {
     myDecoder.decode();
-    if (validHit() == true && hitPoint >= 2) {
+    if (validHit()) {
       validHitResponse();
-      hitPointLEDGreen();
-    }
-    else if (validHit() == true && hitPoint == 1) {
-      validHitResponse();
-      noHitPoint();
+      showHitPointLED(hitPoint);
     }
     else {
-      Serial.print("INVALID HIT 0x");
+      Serial.print(F("INVALID HIT 0x"));
       Serial.println(myDecoder.value, HEX);
     }
   }
