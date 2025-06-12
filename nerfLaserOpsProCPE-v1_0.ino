9abd74221ef06682cbfc7de2f3dc9c8497b2c04e
litanyoffail <122475151+litanyoffail@users.noreply.github.com>
2025-06-12 13:06:24 -0400

    Update and rename nerfLaserOpsProCPE.ino to nerfLaserOpsProCPE-v1_0.ino

--- nerfLaserOpsProCPE-v1_0.ino
+++ nerfLaserOpsProCPE-v1_0.ino
@@ -1,10 +1,11 @@
-#include <Adafruit_CircuitPlayground.h>
 #include <Adafruit_Circuit_Playground.h>
 
+//Less typing is nice.
 #define myReceiver CircuitPlayground.irReceiver
 #define myDecoder CircuitPlayground.irDecoder
+#define strip CircuitPlayground.strip
 
-/* IR hashes for Nerf LaserOps Pro standard game */
+// IR hashes for Nerf LaserOps Pro standard game: AP AlphaPoint/DB DeltaBurst
 #define AP_PURPLE 0x67228B44
 #define AP_RED 0x78653B0E
 #define AP_BLUE 0x2FFEA610
@@ -13,34 +14,37 @@
 #define DB_RED 0xE4469982
 #define DB_BLUE 0x9BE00484
 
-#define PURPLE 0x000A000A
-#define RED 0x000A0000
-#define GREEN 0x00000A00
-#define BLUE 0x0000000A
+const uint32_t alphaPoint[] = {AP_PURPLE, AP_RED, AP_BLUE};
+const uint32_t deltaBurst[] = {DB_PURPLE, DB_RED, AP_BLUE};
 
-const char* teamName[] = {"PURPLE", "RED", "BLUE"};
-const uint32_t teamColor[] = {PURPLE, RED, BLUE};
+//Color codes for LED strip
+#define PURPLE 0xFF00FF
+#define RED 0xFF0000
+#define GREEN 0x00FF00
+#define BLUE 0x0000FF
+#define YELLOW 0xFFFF00
+#define WHITE 0xFFFFFF
 
-Adafruit_CPlay_NeoPixel strip = Adafruit_CPlay_NeoPixel(10, CPLAY_NEOPIXELPIN, NEO_GRB + NEO_KHZ800);
+const int animationPixelR[] = {0, 1, 2, 3, 4};
+const int animationPixelL[] = {9, 8, 7, 6, 5};
 
 // Purple Team = 0
 // Red Team = 1
 // Blue Team = 2
 int teamOwner = 0;
 int teamEnemy = 0;
+const uint32_t teamColor[] = {PURPLE, RED, BLUE};
+const char* teamName[] = {"PURPLE", "RED", "BLUE"};
 
-// Hit Point Config
+// Hit Point settings
 #define HIT_POINT_BASE 10
-int HIT_POINT_MULTIPLIER = 1;
-int hitPoint = HIT_POINT_BASE * HIT_POINT_MULTIPLIER;
+int HIT_MULTIPLIER = 1;
+int hitPoint = HIT_POINT_BASE * HIT_MULTIPLIER;
 
 // Hits will only be valid from opposing team if Red or Blue, or any Purple
 // Prevents Red/Blue from affecting Purple and vice versa to copy stock behaviour.
-// Maybe there is a game mode that can be tied to modyfing this behaviour somehow?
 bool validHit() {
   myDecoder.decode();
-  const uint32_t alphaPoint[] = {AP_PURPLE, AP_RED, AP_BLUE};
-  const uint32_t deltaBurst[] = {DB_PURPLE, DB_RED, AP_BLUE};
 
   return myDecoder.protocolNum == UNKNOWN && 
     (myDecoder.value == alphaPoint[teamEnemy] ||
@@ -48,78 +52,71 @@ bool validHit() {
     hitPoint > 0;
 }
 
+//Do things when hit with a valid blaster.
 void validHitResponse() {
   --hitPoint;
   Serial.print(F("VALID HIT "));
   Serial.print(teamName[teamEnemy]);
   Serial.print(F(" 0x"));
   Serial.println(myDecoder.value, HEX);
-  clearLED();
-  setLED(teamColor[teamEnemy], 0);
+  CircuitPlayground.clearPixels();
+  strip.fill(teamColor[teamEnemy], 0, strip.numPixels());
+  strip.show();
   delay(240);
   showHitPoint();
 }
 
 // Cycle teamOwner variable between teams and show a boot-up animation to indicate teamOwner color when Right Button is pressed.
 void teamSwitchButton() {
-  hitPoint = HIT_POINT_BASE * HIT_POINT_MULTIPLIER;
-  if (++teamOwner > 2) {
-    teamOwner = 0;
-  }
-  if (--teamEnemy < 0) {
-    teamEnemy = 2;
-  }
-  clearLED();
-  setLED(teamColor[teamOwner], 100);
+  HIT_MULTIPLIER = 1;
+  hitPoint = HIT_POINT_BASE * HIT_MULTIPLIER;
+  teamOwner = ++teamOwner % 3;
+  teamEnemy = (--teamEnemy + 3) % 3;
+  CircuitPlayground.clearPixels();
+  setLED(teamColor[teamOwner], 200);
   showHitPoint();
 }
 
 // Cycle between 10/20/30 Hit Points.
 void modeButton() {
-  if (++HIT_POINT_MULTIPLIER < 3) {
-    HIT_POINT_MULTIPLIER = 1;
-  }
+  CircuitPlayground.clearPixels();
+  HIT_MULTIPLIER = HIT_MULTIPLIER++ % 3 +1;
+  hitPoint = HIT_POINT_BASE * HIT_MULTIPLIER;
+  strip.fill(WHITE, 1, HIT_MULTIPLIER);
+  strip.show();
+  delay(420);
+  showHitPoint();
 }
 
 //Show HP on Neopixel LED
 void showHitPoint() {
-  clearLED();
-  for (int i=0; i<hitPoint; i++) {
-    strip.setPixelColor(i, 0x00, 0x0A, 0x00);
-  }
+  CircuitPlayground.clearPixels();
+  strip.fill(GREEN, 0, hitPoint);
   strip.show();
 }
 
-//Set all Neopixel LED, with optional animation delay.
+//Set all Neopixel LED, with optional animation delay; escape animations with the buttons.
 void setLED (uint32_t color, int animation_delay) {
-  uint8_t r = (color >> 16) & 0x0A;
-  uint8_t g = (color >> 8) & 0x0A;
-  uint8_t b = color & 0x0A;
-  for (int i=0; i<hitPoint; i++) { 
-    strip.setPixelColor(i, r, g, b);
+ CircuitPlayground.clearPixels();
+  for (int i=0; i<strip.numPixels(); i++) { 
+    strip.setPixelColor(animationPixelR[i], color);
+    strip.setPixelColor(animationPixelL[i], color);
     strip.show();
     delay(animation_delay);
-  }
-}
-
-//Clears all LEDs.
-void clearLED() {
-  strip.clear();
-  strip.show();
-}
-
-//Loops when HP is 0, and you can hold the teamSwitchButton to reset HP and escape the loop.
-void dead() {
-  clearLED();
-  for (int i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, 0x0A, 0x0A, 0x00);
     if (CircuitPlayground.rightButton()) {
       teamSwitchButton();
       return;
     }
-    strip.show();
-    delay(100);
+    if (CircuitPlayground.rightButton()) {
+      modeButton();
+      return;
+    }
   }
+}
+
+//Loops when hitPoints is 0; you can hold the teamSwitchButton/modeButton to reset HP and escape the loop.
+void dead() {
+  setLED(YELLOW, 420);
 }
 
 void setup() {
@@ -128,9 +125,9 @@ void setup() {
 //  while (!Serial); //delay to wait for serial port
   myReceiver.enableIRIn(); // Start the receiver
   Serial.println(F("Ready to receive IR signals"));
-  strip.begin();
-  clearLED();
-  setLED(teamColor[teamOwner], 100);
+  strip.setBrightness(12);
+  CircuitPlayground.clearPixels();
+  setLED(teamColor[teamOwner], 200);
   showHitPoint();
 }
 
@@ -140,7 +137,7 @@ void loop() {
     dead();
   }
 
-  //Initialize IR Receiver once it gets a signal and reset IR Receiver.
+  //Initialize IR Receiver once it gets a signal and reset IR Receiver. 
   if (myReceiver.getResults()) {
     if (validHit() == true) {
       validHitResponse();
